bfd345661806eab497350ec381a8afdf5758012d
dannybackx <dannybackx@users.noreply.github.com>
2016-11-11 12:22:29 +0100

    I believe this interprets the port number input more accurately. Also fixed an indenting problem at the end of the file.

--- esp-link/cgitelnet.c
+++ esp-link/cgitelnet.c
@@ -34,55 +34,49 @@ int ICACHE_FLASH_ATTR cgiTelnetSet(HttpdConnData *connData) {
     return HTTPD_CGI_DONE; // Connection aborted
   }
 
-  int8_t ok = 0;
+  int8_t ok0, ok1;
   uint16_t port0, port1;
-  ok |= getUInt16Arg(connData, "port0", &port0);
-  ok |= getUInt16Arg(connData, "port1", &port1);
+  ok0 = getUInt16Arg(connData, "port0", &port0);
+  ok1 = getUInt16Arg(connData, "port1", &port1);
+  os_printf("cgiTelnetSet ok0 %d ok1 %d port0 %d port1 %d\n", ok0, ok1, port0, port1);
 
-  if (ok <= 0) { //If we get at least one good value, this should be >= 1
-    ets_sprintf(buf, "Unable to fetch telnet ports.\n Received: port0=%d port1=%d\n",
-	  flashConfig.telnet_port0, flashConfig.telnet_port1);
+  if (ok0 <= 0 && ok1 <= 0) { //If we get at least one good value, this should be >= 1
+    ets_sprintf(buf, "Unable to fetch telnet ports. Previous %d %d  Received: port0=%d port1=%d\n",
+	  flashConfig.telnet_port0, flashConfig.telnet_port1, port0, port1);
     os_printf(buf);
     errorResponse(connData, 400, buf);
     return HTTPD_CGI_DONE;
   }
 
-  if (ok > 0) {
-    // fill both port variables from flash or ajax provided value
-    if (!port0) port0 = flashConfig.telnet_port0;
-    if (!port1) port1 = flashConfig.telnet_port1;
-  
-    // check whether ports are different
-    if (port0 == port1) {
+  if (ok0 == 1) flashConfig.telnet_port0 = port0;
+  if (ok1 == 1) flashConfig.telnet_port1 = port1;
+
+  // check whether ports are different
+  if (flashConfig.telnet_port0 == flashConfig.telnet_port1) {
       os_sprintf(buf, "Ports cannot be the same.\n Tried to set: port0=%d port1=%d\n",
         flashConfig.telnet_port0, flashConfig.telnet_port1);
       os_printf(buf);
       errorResponse(connData, 400, buf);
       return HTTPD_CGI_DONE;
-    }
-
-    // we're good, set flashconfig
-    flashConfig.telnet_port0 = port0;
-    flashConfig.telnet_port1 = port1;
-    os_printf("Telnet ports changed: port0=%d port1=%d\n",
-	  flashConfig.telnet_port0, flashConfig.telnet_port1);
-
-    // save to flash
-    if (configSave()) {
-      httpdStartResponse(connData, 204);
-      httpdEndHeaders(connData);
-    } else {
-      httpdStartResponse(connData, 500);
-      httpdEndHeaders(connData);
-      httpdSend(connData, "Failed to save config", -1);
-    }
-    
-    // apply the changes
-    serbridgeInit();
-    serbridgeStart(0, flashConfig.telnet_port0, flashDefault.telnet_port0mode);
-    serbridgeStart(1, flashConfig.telnet_port1, flashDefault.telnet_port1mode);
-  
   }
+
+  os_printf("Telnet ports changed: port0=%d port1=%d\n",
+    flashConfig.telnet_port0, flashConfig.telnet_port1);
+
+  // save to flash
+  if (configSave()) {
+    httpdStartResponse(connData, 204);
+    httpdEndHeaders(connData);
+  } else {
+    httpdStartResponse(connData, 500);
+    httpdEndHeaders(connData);
+    httpdSend(connData, "Failed to save config", -1);
+  }
+    
+  // apply the changes
+  serbridgeInit();
+  serbridgeStart(0, flashConfig.telnet_port0, flashDefault.telnet_port0mode);
+  serbridgeStart(1, flashConfig.telnet_port1, flashDefault.telnet_port1mode);
 
   return HTTPD_CGI_DONE;
 }
@@ -104,17 +98,16 @@ static char *portMode2string(int8_t m) { //Should we put this into flash?
    return portMode[m];
  }
  
- // print various Telnet information into json buffer
- int ICACHE_FLASH_ATTR printTelnetSecurity(char *buff) {
-   int len;
+// print various Telnet information into json buffer
+int ICACHE_FLASH_ATTR printTelnetSecurity(char *buff) {
+  int len;
  
-   len = os_sprintf(buff,
-     "{ \"port0mode\": \"%s\", \"port0portnumber\": \"%d\", \"port0pwd\": \"%s\", "
-     "\"port1mode\": \"%s\", \"port1portnumber\": \"%d\", \"port1pwd\": \"%s\" }",
-     portMode2string(flashConfig.telnet_port0mode), flashConfig.telnet_port0, flashConfig.telnet_port0pass,
-     portMode2string(flashConfig.telnet_port1mode), flashConfig.telnet_port1, flashConfig.telnet_port1pass
-   );
+  len = os_sprintf(buff,
+    "{ \"port0mode\": \"%s\", \"port0portnumber\": \"%d\", \"port0pwd\": \"%s\", "
+    "\"port1mode\": \"%s\", \"port1portnumber\": \"%d\", \"port1pwd\": \"%s\" }",
+    portMode2string(flashConfig.telnet_port0mode), flashConfig.telnet_port0, flashConfig.telnet_port0pass,
+    portMode2string(flashConfig.telnet_port1mode), flashConfig.telnet_port1, flashConfig.telnet_port1pass
+  );
  
-   return len;
- }
- 
+  return len;
+}
