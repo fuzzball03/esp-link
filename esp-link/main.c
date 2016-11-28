848c9733cb871aef82a5afbb147991be6787b32d
Alex <alex.craigslist03@gmail.com>
2016-11-28 15:35:46 -0600

    Test authBasic function
    This should require you to enter a username & password to go into the WiFi pages on the esp.
    
    Use the following user & pass for testing
    admin s3cr3t

--- esp-link/main.c
+++ esp-link/main.c
@@ -54,6 +54,23 @@ bool ICACHE_FLASH_ATTR check_memleak_debug_enable(void)
 }
 #endif
 
+//Function that tells the authentication system what users/passwords live on the system.
+//This is disabled in the default build; if you want to try it, enable the authBasic line in
+//the builtInUrls below.
+int myPassFn(HttpdConnData *connData, int no, char *user, int userLen, char *pass, int passLen) {
+	if (no==0) {
+		os_strcpy(user, "admin");
+		os_strcpy(pass, "s3cr3t");
+		return 1;
+//Add more users this way. Check against incrementing no for each user added.
+//	} else if (no==1) {
+//		os_strcpy(user, "user1");
+//		os_strcpy(pass, "something");
+//		return 1;
+	}
+	return 0;
+}
+
 /*
 This is the main url->function dispatching data struct.
 In short, it's a struct with various URLs plus their handlers. The handlers can
@@ -217,21 +234,3 @@ user_init(void) {
   system_show_malloc();
 #endif
 }
-
-//Function that tells the authentication system what users/passwords live on the system.
-//This is disabled in the default build; if you want to try it, enable the authBasic line in
-//the builtInUrls below.
-int myPassFn(HttpdConnData *connData, int no, char *user, int userLen, char *pass, int passLen) {
-	if (no==0) {
-		os_strcpy(user, "admin");
-		os_strcpy(pass, "s3cr3t");
-		return 1;
-//Add more users this way. Check against incrementing no for each user added.
-//	} else if (no==1) {
-//		os_strcpy(user, "user1");
-//		os_strcpy(pass, "something");
-//		return 1;
-	}
-	return 0;
-}
-
