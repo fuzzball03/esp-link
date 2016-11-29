d7f58e2f66b7efee99a409698476f4d03eb898d5
dannybackx <dannybackx@users.noreply.github.com>
2016-11-29 04:59:39 +0100

    Keep this function, as the one from the library appears to crash.

--- httpd/auth.c
+++ httpd/auth.c
@@ -24,17 +24,20 @@ int ICACHE_FLASH_ATTR authBasic(HttpdConnData *connData) {
 	char userpass[AUTH_MAX_USER_LEN+AUTH_MAX_PASS_LEN+2];
 	char user[AUTH_MAX_USER_LEN];
 	char pass[AUTH_MAX_PASS_LEN];
+
+// os_printf("authBasic()\n");
 	if (connData->conn==NULL) {
 		//Connection aborted. Clean up.
 		return HTTPD_CGI_DONE;
 	}
 
 	r=httpdGetHeader(connData, "Authorization", hdr, sizeof(hdr));
+// os_printf("httpdGetHeader -> %d {%s}\n", r, hdr);
 	if (r && strncmp(hdr, "Basic", 5)==0) {
-		r=base64_decode(strlen(hdr)-6, hdr+6, sizeof(userpass), (unsigned char *)userpass);
+		r=my_base64_decode(strlen(hdr)-6, hdr+6, sizeof(userpass), (unsigned char *)userpass);
 		if (r<0) r=0; //just clean out string on decode error
 		userpass[r]=0; //zero-terminate user:pass string
-//		os_printf("Auth: %s\n", userpass);
+// os_printf("Auth: %s\n", userpass);
 		while (((AuthGetUserPw)(connData->cgiArg))(connData, no,
 				user, AUTH_MAX_USER_LEN, pass, AUTH_MAX_PASS_LEN)) {
 			//Check user/pass against auth header
@@ -43,6 +46,7 @@ int ICACHE_FLASH_ATTR authBasic(HttpdConnData *connData) {
 					userpass[strlen(user)]==':' &&
 					os_strcmp(userpass+strlen(user)+1, pass)==0) {
 				//Authenticated. Yay!
+os_printf("Authenticated as '%s', Yay !\n", user);
 				return HTTPD_CGI_AUTHENTICATED;
 			}
 			no++; //Not authenticated with this user/pass. Check next user/pass combo.
@@ -56,6 +60,7 @@ int ICACHE_FLASH_ATTR authBasic(HttpdConnData *connData) {
 	httpdEndHeaders(connData);
 	httpdSend(connData, forbidden, -1);
 	//Okay, all done.
+os_printf("Not authenticated !\n");
 	return HTTPD_CGI_DONE;
 }
 
